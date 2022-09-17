9c90bb4eb2226404f187b5dcfad711d4062858ca
nirmalbafna <113161791+nirmalbafna@users.noreply.github.com>
2022-09-17 11:50:47 +0530

    Create const_cast.cpp

---
+++ const_cast.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void printArray(double* a, int size, string name) {
+    cout << "Array " << name << ": ";
+    for (int i = 0; i < size; i++)
+        cout <<*(a + i) << " ";
+    cout << endl;
+}
+int main() {
+    const int SIZE = 8;
+    const double array1[SIZE] = {0};
+    volatile double array2[SIZE] = {0};
+    printArray(const_cast<double*> (array1), SIZE, "one");
+    printArray(const_cast<double*> (array2), SIZE, "two");
+    cout << "\nRemoving const and volatile modifiers, modifying...\n";
+    double *modifyingPtr = 0;
+    for (int i = 0; i < SIZE; i++) {
+        modifyingPtr = const_cast<double*> (array1 + i);
+        *modifyingPtr = i + 0.1 + 0.1 * i;
+        modifyingPtr = const_cast<double*> (array2 + i);
+        *modifyingPtr = i + 0.3 + 0.3 * i;
+    }
+    cout << "Modified arrays:\n";
+    printArray(const_cast<double*> (array1), SIZE, "one");
+    printArray(const_cast<double*> (array2), SIZE, "two");
+return 0;
+}
